7579138f4ae370c0691b476ed98be7bcb835adf3
Nathaniel Williams <yezzirmoney@gmail.com>
2025-04-05 15:37:45 -0400

    Add date of birth and profile photo to UserSeeder
    This update includes two new fields: `date_of_birth` and `profile_photo_url` for seeded users. These additions enhance the completeness of user profiles in the database.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,6 +20,8 @@ class UserSeeder extends Seeder
             'password' => Hash::make('propel42'),
             'location' => 'Boca Raton, FL, USA',
             'phone_number' => '(330) 458-9393',
+            'date_of_birth' => '1992-07-08',
+            'profile_photo_url' => 'https://media.licdn.com/dms/image/v2/C4D03AQElW2erCpRQww/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1565713960651?e=1749081600&v=beta&t=wdsEcXFxcV7fyUGMVyuF7ZUBfwo-QcspH2kzrO8_9Ps',
             'linkedin_url' => 'https://linkedin.com/in/attentivnate/',
             'github_url' => 'https://github.com/gluebag',
             'personal_website_url' => 'https://attentiv.dev',
